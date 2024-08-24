edbbad26c0e0fac399e7c374b9f511ce48c9ce9f
Carlos Monge <cmonge@jackdevelopers.com>
2024-08-24 13:15:30 -0600

    Fix user agent request

--- l10n_cr_hacienda_info_query/models/res_partner.py
+++ l10n_cr_hacienda_info_query/models/res_partner.py
@@ -60,7 +60,7 @@ class ResPartner(models.Model):
             end_point = url_base + 'identificacion=' + cedula
 
             headers = {'content-type': 'application/json',
-                       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36', }
+                       'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36' }
             try:
                 peticion = requests.get(end_point, headers=headers, timeout=10)
 
